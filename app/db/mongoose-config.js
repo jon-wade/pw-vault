0d4e1e0ec9f09fae3fb6d1265a9c521e1b5cecd2
jon-wade <jonwadeuk@gmail.com>
2016-07-17 10:24:06 +0100

    deactivate mongo-test environment before deployment

--- app/db/mongoose-config.js
+++ app/db/mongoose-config.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var environment = {
     production: "",
     development: "mongodb://localhost:27017/pw-vault-dev",
-    test: "mongodb://localhost:27018/pw-vault-test"
+    //test: "mongodb://localhost:27018/pw-vault-test"
 };
 
 var User = mongoose.Schema({
@@ -20,14 +20,14 @@ var Manager = mongoose.Schema({
     password: {type: String, minlength: 44, required: true}
 });
 
-var test = mongoose.createConnection(environment.test);
+//var test = mongoose.createConnection(environment.test);
 //console.log('test=', typeof test);
 //console.log('test keys=', Object.keys(test));
 var dev = mongoose.createConnection(environment.development);
 //console.log('dev=', typeof dev);
 //console.log('dev keys=', Object.keys(dev));
 
-exports.userTest = test.model('testUser', User);
+//exports.userTest = test.model('testUser', User);
 //console.log('userTest=', typeof exports.userTest());
 //console.log('userTest keys=', Object.keys(exports.userTest));
 
@@ -35,7 +35,7 @@ exports.userDev = dev.model('devUser', User);
 //console.log('userTest=', typeof exports.userDev());
 //console.log('userTest keys=', Object.keys(exports.userDev));
 
-exports.managerTest = test.model('testManager', Manager);
+//exports.managerTest = test.model('testManager', Manager);
 
 exports.managerDev = dev.model('devManager', Manager);
 
